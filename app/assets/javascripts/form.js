b33dd422ab785e23079e73da51297f2c107953ae
Felipe <felipe.claro654@gmail.com>
2016-11-08 12:27:53 -0200

    Corrige traduções. Utiliza js para resetar inputs com erro.

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -2,4 +2,10 @@ $(function () {
     $(".container .calendario").datetimepicker({
         format: "DD/MM/YYYY"
     });
+
+    $(".has-error input").focus(function () {
+        var parent = $(this).parents(".form-group");
+        parent.removeClass('has-error');
+        parent.find(".help-block").remove();
+    });
 });
