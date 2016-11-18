d726ba5eccbf6f413dd84c831d2b78503704bd00
Felipe <felipe.claro654@gmail.com>
2016-11-18 08:16:39 -0200

    Atualiza metodo de renderização da tabela autor

--- app/controllers/autors_controller.rb
+++ app/controllers/autors_controller.rb
@@ -6,31 +6,7 @@ class AutorsController < ApplicationController
     def show_autor_table
         if params[:autor]
             @autor = Autor.find(params[:autor])
-            @inicio = @autor.periodo_inicial
-
-            @periodos = retorna_periodos(@autor)
-
-            @tabela = @autor.processo.tabela_atualizacao.nome
-            range_anos = (@autor.periodo_final.year) - (@autor.periodo_inicial.year)
-            anos = []
-
-            (0..range_anos).each_with_index do |r, index|
-                anos.push("\t"+(@autor.periodo_inicial.year + index).to_s + "\t")
-            end
-            @meses = month_difference(@autor.periodo_inicial, @autor.periodo_final)
-
-            if @tabela == "PCA-E"
-                @tabela = TabelaOpv.where("ano IN (?)", anos)
-                @indice_atualizacao =
-                    TabelaOpv.where(ano: "\t"+(@autor.processo.data_base.year).to_s + "\t").
-                    where(mes: "\t"+(@autor.processo.data_base.strftime("%m")) + "\t")[0].valor
-            else
-                @tabela = TabelaJudicial.where("ano IN (?)", anos)
-                @indice_atualizacao =
-                    TabelaJudicial.where(ano: "\t"+(@autor.processo.data_base.year).to_s + "\t").
-                    where(mes: "\t"+(@autor.processo.data_base.strftime("%m")) + "\t")[0].valor
-            end
-
+            @indice_atualizacao = retorna_indice(@autor.processo.data_base, @autor.processo.tabela_atualizacao.nome)
             @pagamentos = Pagamento.where(autor_id: @autor.id)
 
             render 'autor_table'
--- app/controllers/processos_controller.rb
+++ app/controllers/processos_controller.rb
@@ -63,32 +63,6 @@ class ProcessosController < ApplicationController
         end
     end
 
-    def retorna_indice_front
-        if params[:tabela] == "PCA-E"
-            @indice_tabela =
-                TabelaOpv.where(ano: "\t" + params[:data_base][6..9] + "\t").
-                where(mes: "\t" + params[:data_base][3..4] + "\t")[0].valor
-        else
-            @indice_tabela =
-                TabelaJudicial.where(ano: "\t" + params[:data_base][6..9] + "\t").
-                where(mes: "\t" + params[:data_base][3..4] + "\t")[0].valor
-        end
-            render json: @indice_tabela
-    end
-
-    def retorna_indice(data_base, tabela)
-        if data_base == "PCA-E"
-            @indice_tabela =
-                TabelaOpv.where(ano: "\t" + data_base.year.to_s + "\t").
-                where(mes: "\t" + data_base.strftime("%m") + "\t")[0].valor
-        else
-            @indice_tabela =
-                TabelaJudicial.where(ano: "\t" + data_base.year.to_s + "\t").
-                where(mes: "\t" + data_base.strftime("%m") + "\t")[0].valor
-        end
-        @indice_tabela
-    end
-
     def update_autores(processo)
         processo.autors.each do |a|
             @pagamentos = Pagamento.where(autor_id: a.id)
