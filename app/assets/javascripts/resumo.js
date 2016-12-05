aa598ce6b0a5386389921bde5dd403aa0a387798
Felipe <felipe.claro654@gmail.com>
2016-12-05 17:51:27 -0200

    Corrige multiplas chamadas para print de resumo

--- app/assets/javascripts/resumo.js
+++ app/assets/javascripts/resumo.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(function () {
     $(".total-assistencia").html($(".total-assistencia-autor").html());
 
     $(document).on("click","#print_resumo", function() {
@@ -12,4 +12,21 @@ $(document).on('turbolinks:load', function() {
             },500);
         }, 500);
     });
+});
+
+
+$(document).on("click","#print_autor_table", function() {
+    $(".container").css("visibility", "hidden");
+    Autor_Table.print_autor();
+    setTimeout(function () {
+        $(".print-container").css("visibility", "visible");
+        window.print();
+        $(".print-container").css("visibility", "hidden");
+        $(".print-container").switchClass("print-container", "container");
+        $(".container-autor-table").toggleClass("width-percent-70");
+        $(".container-autor-table").toggleClass("margin-left-90");
+        setTimeout(function () {
+            $(".container").css("visibility", "visible");
+        },500);
+    }, 500);
 });
