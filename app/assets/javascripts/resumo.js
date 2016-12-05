d8d993e1d609633ae3dab232361ac12f0be21a0a
Felipe <felipe.claro654@gmail.com>
2016-12-05 18:39:37 -0200

    Insere informacoes no resumo

--- app/assets/javascripts/resumo.js
+++ app/assets/javascripts/resumo.js
@@ -1,32 +1,17 @@
 $(function () {
-    $(".total-assistencia").html($(".total-assistencia-autor").html());
+    setTimeout(function () {
+        $(".total-assistencia").html($(".total-assistencia-autor").html());
+    },700);
 
     $(document).on("click","#print_resumo", function() {
-        $(".container").css("visibility", "hidden");
+        //$(".container").css("visibility", "hidden");
         $(".resumo-page").addClass("print-resumo");
-        setTimeout(function () {
+        /*setTimeout(function () {
             $(".container").css("visibility", "visible");
             window.print();
             setTimeout(function () {
                 $(".resumo-page").removeClass("print-resumo");
             },500);
-        }, 500);
+        }, 500);*/
     });
-});
-
-
-$(document).on("click","#print_autor_table", function() {
-    $(".container").css("visibility", "hidden");
-    Autor_Table.print_autor();
-    setTimeout(function () {
-        $(".print-container").css("visibility", "visible");
-        window.print();
-        $(".print-container").css("visibility", "hidden");
-        $(".print-container").switchClass("print-container", "container");
-        $(".container-autor-table").toggleClass("width-percent-70");
-        $(".container-autor-table").toggleClass("margin-left-90");
-        setTimeout(function () {
-            $(".container").css("visibility", "visible");
-        },500);
-    }, 500);
 });
