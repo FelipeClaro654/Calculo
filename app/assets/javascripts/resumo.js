68522cecd8e68657fa1bb6714bb3f66fb2ff4f4b
Felipe <felipe.claro654@gmail.com>
2016-12-06 17:09:16 -0200

    Tamanho da impresso do resumo

--- app/assets/javascripts/resumo.js
+++ app/assets/javascripts/resumo.js
@@ -114,14 +114,14 @@ $(document).on("turbolinks:load", function () {
 $(function () {
     $(document).on("click","#print_resumo", function() {
         $(".container").css("visibility", "hidden");
-        $(".resumo-page").toggleClass("print-resumo");
+        $("body").toggleClass("print-resumo");
         $(".resumo-page").toggleClass("width-percent-70");
         setTimeout(function () {
             $(".container").css("visibility", "visible");
             window.print();
             setTimeout(function () {
                 $(".resumo-page").toggleClass("width-percent-70");
-                $(".resumo-page").toggleClass("print-resumo");
+                $("body").toggleClass("print-resumo");
             },500);
         }, 500);
     });
